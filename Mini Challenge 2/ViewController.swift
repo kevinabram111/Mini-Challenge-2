56fa718e21d514639fc647ee0e0cfb706607d6e5
Kevin <kevinabram1000@gmail.com>
2019-07-16 17:57:41 +0700

    Initial Commit

---
+++ Mini Challenge 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mini Challenge 2
+//
+//  Created by Kevin Abram on 16/07/19.
+//  Copyright © 2019 Kevin Abram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
